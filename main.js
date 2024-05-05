a61bcf9208712a857ef84d88d26fddcc8bad4c5a
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2024-05-06 00:50:38 +0200

    Write size function

--- main.js
+++ main.js
@@ -26,6 +26,16 @@ class LinkedList {
         this.root = new Node(value, this.root);
     }
 
+    size() {
+        let currentNode = this.root;
+        let counter = 0;
+        while (currentNode instanceof Node) {
+            currentNode = currentNode.nextNode;
+            counter++;
+        }
+        return counter
+    }
+
     printValues() {
         let currentNode = this.root
         while (currentNode) {
@@ -42,17 +52,20 @@ let linkedList = new LinkedList()
 
 linkedList.append(1)
 linkedList.append(2)
-linkedList.append(3)
-linkedList.append(4)
-linkedList.append(5)
-linkedList.append(6)
-linkedList.append(7)
-linkedList.append(8)
-linkedList.append(9)
-linkedList.append(10)
-linkedList.append(11)
-linkedList.append(12)
+// linkedList.append(3)
+// linkedList.append(4)
+// linkedList.append(5)
+// linkedList.append(6)
+// linkedList.append(7)
+// linkedList.append(8)
+// linkedList.append(9)
+// linkedList.append(10)
+// linkedList.append(11)
+// linkedList.append(12)
 linkedList.prepend(13)
+linkedList.prepend(14)
 
 linkedList.printValues()
+
+console.log(linkedList.size())
 
